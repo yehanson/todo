7fafa456669ea14dc37713258037ba2fa5f0f22c
yehanson <80563592+yehanson@users.noreply.github.com>
2021-04-20 16:24:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
-const inpt= document.querySelector('#inField');
-const btn= document.querySelector('#addBtn');
-const li= document.querySelector('#list');
-let array;
+const input= document.querySelector('.input');
+const btn= document.querySelector('.btn');
+const container= document.querySelector('.container');
 
 btn.addEventListener('click', add);
+container.addEventListener('click', del);
 
 window.onload= display();
 
+function add(){
+    if(input.value != 0){
+        const newDiv= document.createElement('div');
+        container.appendChild(newDiv);
+        
+        const newLi= document.createElement('li');
+        newLi.innerHTML= input.value;
+        newDiv.appendChild(newLi);
+        
+        const newBtn= document.createElement('button');
+        newBtn.innerHTML= 'x';
+        newBtn.classList.add('delBtn');
+        newDiv.appendChild(newBtn);
+
+        setLocal();
+
+        input.value= '';
+    };
+};
+
+function del(e){
+    if(e.target.classList== 'delBtn'){
+        e.target.parentElement.remove();
+        removeLocal(e);
+    };
+};
+
+function setLocal(){
+    let array;
+    if(localStorage.getItem('todo')== null){
+        array= [];
+    }else{
+        array= JSON.parse(localStorage.getItem('todo'));
+    };
+    
+    array.push(input.value);
+    localStorage.setItem('todo', JSON.stringify(array));
+};
+
 function display(){
+    let array;
     if(localStorage.getItem('todo')== null){
         array= [];
     }else{
@@ -15,21 +55,28 @@ function display(){
     };
 
     for(i= 0; i<array.length; i++){
-        li.innerHTML+= array[i] + `<br>`;
+        const newDiv= document.createElement('div');
+        container.appendChild(newDiv);
+        
+        const newLi= document.createElement('li');
+        newLi.innerHTML= array[i];
+        newDiv.appendChild(newLi);
+        
+        const newBtn= document.createElement('button');
+        newBtn.innerHTML= 'x';
+        newBtn.classList.add('delBtn');
+        newDiv.appendChild(newBtn);
     };
 };
 
-function add(){
-    li.innerHTML= '';
-
-    if(inpt.value.trim()!=0){
-        array.push(inpt.value.trim());
-        localStorage.setItem('todo', JSON.stringify(array));
-        inpt.value= '';
-        display();
+function removeLocal(e){
+    let array;
+    if(localStorage.getItem('todo')== null){
+        array= [];
+    }else{
+        array= JSON.parse(localStorage.getItem('todo'));
     };
-};
-
-function del(){
-
+    
+    array.splice(array.indexOf(e.target.parentElement.children[0].innerHTML), 1);
+    localStorage.setItem('todo', JSON.stringify(array));
 };
