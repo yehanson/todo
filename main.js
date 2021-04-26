a1de80d37ad90bf842d0a68073c78bf0a064c6bc
HANSON <80563592+yehanson@users.noreply.github.com>
2021-04-26 16:30:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const input= document.querySelector('.input');
 const btn= document.querySelector('.btn');
 const container= document.querySelector('.container');
+const ed= document.querySelector('.ed');
+let array;
+let placeholder;
 
 btn.addEventListener('click', add);
-container.addEventListener('click', del);
+container.addEventListener('click', button);
+ed.addEventListener('click', button);
 
 window.onload= display();
 
@@ -15,11 +19,16 @@ function add(){
         const newLi= document.createElement('li');
         newLi.innerHTML= input.value;
         newDiv.appendChild(newLi);
+
+        const newBtn1= document.createElement('button');
+        newBtn1.innerHTML= 'Edit';
+        newBtn1.classList.add('edBtn');
+        newDiv.appendChild(newBtn1);
         
-        const newBtn= document.createElement('button');
-        newBtn.innerHTML= 'x';
-        newBtn.classList.add('delBtn');
-        newDiv.appendChild(newBtn);
+        const newBtn2= document.createElement('button');
+        newBtn2.innerHTML= 'x';
+        newBtn2.classList.add('delBtn');
+        newDiv.appendChild(newBtn2);
 
         setLocal();
 
@@ -27,15 +36,35 @@ function add(){
     };
 };
 
-function del(e){
-    if(e.target.classList== 'delBtn'){
+function button(e){
+    if(e.target.classList== 'edBtn'){
+        if(ed.hasChildNodes()){
+            ed.innerHTML= '';
+        };
+        const editDiv= document.createElement('div');
+        ed.appendChild(editDiv);
+        
+        const newInput= document.createElement('input');
+        placeholder= array.indexOf(e.target.parentElement.children[0].innerHTML);
+        newInput.value= e.target.parentElement.children[0].innerHTML;
+        editDiv.appendChild(newInput);
+
+        const finBtn= document.createElement('button');
+        finBtn.innerHTML= 'Finish Edit';
+        finBtn.classList.add('finBtn');
+        editDiv.appendChild(finBtn);
+    }else if(e.target.classList== 'delBtn'){
         e.target.parentElement.remove();
         removeLocal(e);
+    }else if(e.target.classList== 'finBtn'){
+        array[placeholder]= e.target.parentElement.children[0].value;
+        console.log(array);
+        localStorage.setItem('todo', JSON.stringify(array));
+        window.location.reload();
     };
 };
 
 function setLocal(){
-    let array;
     if(localStorage.getItem('todo')== null){
         array= [];
     }else{
@@ -47,7 +76,6 @@ function setLocal(){
 };
 
 function display(){
-    let array;
     if(localStorage.getItem('todo')== null){
         array= [];
     }else{
@@ -61,6 +89,11 @@ function display(){
         const newLi= document.createElement('li');
         newLi.innerHTML= array[i];
         newDiv.appendChild(newLi);
+
+        const newBtn1= document.createElement('button');
+        newBtn1.innerHTML= 'Edit';
+        newBtn1.classList.add('edBtn');
+        newDiv.appendChild(newBtn1);
         
         const newBtn= document.createElement('button');
         newBtn.innerHTML= 'x';
@@ -70,13 +103,12 @@ function display(){
 };
 
 function removeLocal(e){
-    let array;
     if(localStorage.getItem('todo')== null){
         array= [];
     }else{
         array= JSON.parse(localStorage.getItem('todo'));
     };
-    
+
     array.splice(array.indexOf(e.target.parentElement.children[0].innerHTML), 1);
     localStorage.setItem('todo', JSON.stringify(array));
 };
